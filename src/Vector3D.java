c86e1adb3576329f5e1bf2b7a9c6e830f4f2ef03
Ashley Zheng <zheng.as@northeastern.edu>
2025-01-09 20:17:49 -0800

    lab 1

---
+++ src/Vector3D.java
@@ -0,0 +1,28 @@
+public class Vector3D {
+    double x;
+    double y;
+    double z;
+
+    Vector3D(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    double getX() {
+        return x;
+    }
+
+    double getY() {
+        return y;
+    }
+
+    double getZ() {
+        return z;
+    }
+
+    public static void main(String[] args) {
+
+
+    }
+}
